3f4080ae3c7307c10397ec0bf9313ac8a9f5aea8
Bianca Slavov <bianca_slavov@hotmail.com>
2022-10-22 20:52:53 +0200

    Made sure that submitted keywords in the search field are stored - added the needed event listeners, functions and states for this.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchBar(){
+    let [keyword, setKeyword] = useState("");
+
+    function search(event){
+        event.preventDefault();
+        alert(`Searching for ${keyword} photos`);
+    }
+
+    function handleKeywordChange(event){
+        setKeyword(event.target.value);
+    }
+
+
     return(
-        <div>Hello from SearchBar</div>
+        <div className="Searchbar">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeywordChange}/>
+            </form>
+        </div>
     )
 }
