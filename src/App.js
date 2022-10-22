372f1ad13da0cf8f4e89abd7df24989f1ce41ed2
Bianca Slavov <bianca_slavov@hotmail.com>
2022-10-22 16:51:06 +0200

    Installed Bootstrap + created the SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      Search beautiful free high-resolution photos
+      <SearchBar />
       </header>
     </div>
   );
